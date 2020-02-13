0b5d5d3fec862bab887d040d22af511a0ec3fd14
Mathias Petersen <mathias.petersen95@gmail.com>
2020-02-13 16:54:23 +0100

    impl box distance heuristic

--- searchclient_python/searchclient/heuristic.py
+++ searchclient_python/searchclient/heuristic.py
@@ -11,13 +11,27 @@ class Heuristic(metaclass=ABCMeta):
                 if initial_state.goals[row][col] is not None:
                     self.goal_locations.append((row,col,initial_state.goals[row][col]))
   
-    
+
     def h(self, state: 'State') -> 'int':
-        # Straight Line distance
-        for goal in self.goal_locations:
-            a = goal[1] - state.agent_col
-            b = goal[0] - state.agent_row
-            return math.sqrt(math.pow(a, 2)+ math.pow(b,2))
+        # distance from box to nearest goal
+        distances = []
+
+        for row in range(state.MAX_ROW):
+            for col in range(state.MAX_COL):
+                if state.boxes[row][col] is not None:
+                    goals_of_same_type = filter(lambda goal: goal[2].upper() == state.boxes[row][col], self.goal_locations)
+
+                    sorted_distances = sorted(map(lambda goal: math.sqrt(math.pow(goal[1] - state.agent_col, 2)+ math.pow(goal[0] - state.agent_row,2)), goals_of_same_type))
+                    distances.append(sorted_distances.pop())
+
+        return sum(distances)
+        
+    # def h(self, state: 'State') -> 'int':
+    #     # Straight Line distance
+    #     for goal in self.goal_locations:
+    #         a = goal[1] - state.agent_col
+    #         b = goal[0] - state.agent_row
+    #         return math.sqrt(math.pow(a, 2)+ math.pow(b,2))
     
     @abstractmethod
     def f(self, state: 'State') -> 'int': pass
